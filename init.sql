85e04a34a217936367d2de8027f4ef675e8b5e9e
Alex Mapley <alexmapley@gmail.com>
2017-02-23 19:35:23 -0500

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,3 +1,27 @@
+DROP TABLE dogFriends;
+DROP TABLE familyMembers; 
+DROP TABLE relationships;
+DROP TABLE uploadsPhoto;
+DROP TABLE tagsPhoto;  
+DROP TABLE makesStatus; 
+DROP TABLE membersOfClubs; 
+DROP TABLE clubAdmins; 
+DROP TABLE goingToEvent; 
+DROP TABLE ownsDogs; 
+DROP TABLE eventAdmins; 
+DROP TABLE servicesSponsorEvents; 
+DROP TABLE ownerReservesServices; 
+DROP TABLE retailAdmins; 
+DROP TABLE serviceAdmins;
+DROP TABLE dogStatuses;
+DROP TABLE dogPictures;
+DROP TABLE dogClubs;
+DROP TABLE dogEvents;
+DROP TABLE ownerProfiles;
+DROP TABLE retailers;
+DROP TABLE services;
+DROP TABLE dogProfiles;
+
 CREATE TABLE dogProfiles (
 	did INTEGER NOT NULL,
 	name VARCHAR (100),
