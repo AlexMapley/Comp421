ec477d57528ec359bf090ca483b716432d812c92
Alex Mapley <alexmapley@gmail.com>
2017-03-15 21:34:52 -0400

    Add files via upload

--- p3.py
+++ p3.py
@@ -1,6 +1,9 @@
-print "Here are your five option:"
+print "Here are your five options:"
 
-print "Do thing 1"
+1. Select a dog you’d like to query with, either by id or name
+(Currently you are our first dog, Spike (#1)
+
+2. To see all shared connections with another dog, enter a name or id
 print "Do thing 2"
 print "Do thing 3"
 print "Do thing 4"
